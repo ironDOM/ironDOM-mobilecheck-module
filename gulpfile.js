b82854b7d0485b8f0a022e2f54c2034f86f3ec0b
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-12-15 11:00:30 +0100

    Added Gulp performance log task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,119 @@
-var gulp            = require('gulp'),
-    // this is an arbitrary object that loads all gulp plugins in package.json.
+var gulp      = require('gulp'),
     $         = require("gulp-load-plugins")(),
     express   = require('express'),
     path      = require('path'),
     tinylr    = require('tiny-lr'),
     app       = express(),
     server    = tinylr(),
-    deploy    = require('gulp-gh-pages');
+    deploy    = require('gulp-gh-pages'),
+    psi       = require('psi'),
+    fs        = require('fs'),
+    moment    = require('moment'),
+    mkdirp = require('mkdirp'),
+    psisite = 'http://irondom.github.io/ironDOM-class-module/';
+
+var year = moment().get('year'),
+  month = moment().get('month'),
+  day = moment().get('date'),
+  hour = moment().get('hour'),
+  minute = moment().get('minute'),
+  second = moment().get('second'),
+  fullDate = year + '-' + month + '-' + day + '-T' + hour + '-' + minute + '-' + second;
+
+gulp.task('speed-mobile', function (cb) {
+  var options = {
+    // key: key,
+    nokey: 'true',
+    url: psisite,
+    strategy: 'mobile'
+  };
+
+  psi(options, function (err, data) {
+    var perfContent =
+      '# Peformance results of '+ fullDate +': \n' +
+      ' \n' +
+      '- Url: ' + data.id + '\n' +
+      '- Score: ' + data.score + '\n' +
+      '- Page stats: \n' +
+      '- Number Resources: ' + data.pageStats.numberResources + '\n' +
+      '- Total Request Bytes: ' + data.pageStats.totalRequestBytes + '\n' +
+      '- Number Static Resources: ' + data.pageStats.numberStaticResources + '\n' +
+      '- HTML Response Bytes: ' + data.pageStats.htmlResponseBytes + ' bytes \n' +
+      '- CSS Response Bytes: ' + data.pageStats.cssResponseBytes + ' bytes \n' +
+      '- JavaScript Response Bytes: ' + data.pageStats.javascriptResponseBytes + ' bytes \n' +
+      '- Number JavaScript Resources: ' + data.pageStats.numberJsResources + '\n' +
+      '- Number CSS Resources: ' + data.pageStats.numberCssResources + '\n'+
+      '- Avoid Landing Page Redirects: ' + JSON.stringify(data.formattedResults.ruleResults.AvoidLandingPageRedirects.ruleImpact) + '\n' +
+      '- Enable Gzip Compression: ' + JSON.stringify(data.formattedResults.ruleResults.EnableGzipCompression.ruleImpact) + '\n' +
+      '- Leverage Browser Caching: ' + JSON.stringify(data.formattedResults.ruleResults.LeverageBrowserCaching.ruleImpact) + '\n' +
+      '- Main Resource Server Response Time: ' + JSON.stringify(data.formattedResults.ruleResults.MainResourceServerResponseTime.ruleImpact) + '\n' +
+      '- Minify CSS: ' + JSON.stringify(data.formattedResults.ruleResults.MinifyCss.ruleImpact) + '\n' +
+      '- Minify HTML: ' + JSON.stringify(data.formattedResults.ruleResults.MinifyHTML.ruleImpact) + '\n' +
+      '- Minify JavaScript: ' + JSON.stringify(data.formattedResults.ruleResults.MinifyJavaScript.ruleImpact) + '\n' +
+      '- MinimizeRenderBlockingResources: ' + JSON.stringify(data.formattedResults.ruleResults.MinimizeRenderBlockingResources.ruleImpact) + '\n' +
+      '- Optimize Images: ' + JSON.stringify(data.formattedResults.ruleResults.OptimizeImages.ruleImpact) + '\n' +
+      '- Prioritize Visible Content: ' + JSON.stringify(data.formattedResults.ruleResults.PrioritizeVisibleContent.ruleImpact) + '\n';
+
+    //console.log(data.formattedResults.ruleResults.PrioritizeVisibleContent);
+    mkdirp('performance-reports/mobile/', function (err) {
+      if (err) {
+        console.error(err)
+      } else {
+        fs.writeFile('performance-reports/mobile/test-mobile-' + fullDate + '.md', perfContent);
+        console.log('You can find the report: performance-reports/mobile/test-mobile-' + fullDate + '.md');
+      }
+    });
+
+    //console.log(data);
+  });
+});
 
 
-gulp.task('deploy', function () {
-  return gulp.src('./dist/**/*')
-    .pipe(deploy());
+gulp.task('speed-desktop', function (cb) {
+  var options = {
+    // key: key,
+    nokey: 'true',
+    url: psisite,
+    strategy: 'desktop'
+  };
+
+  psi(options, function (err, data) {
+    var perfContent =
+      '# Peformance results of '+ fullDate +': \n' +
+      ' \n' +
+      '- Url: ' + data.id + '\n' +
+      '- Score: ' + data.score + '\n' +
+      '- Page stats: \n' +
+      '- Number Resources: ' + data.pageStats.numberResources + '\n' +
+      '- Total Request Bytes: ' + data.pageStats.totalRequestBytes + '\n' +
+      '- Number Static Resources: ' + data.pageStats.numberStaticResources + '\n' +
+      '- HTML Response Bytes: ' + data.pageStats.htmlResponseBytes + ' bytes \n' +
+      '- CSS Response Bytes: ' + data.pageStats.cssResponseBytes + ' bytes \n' +
+      '- JavaScript Response Bytes: ' + data.pageStats.javascriptResponseBytes + ' bytes \n' +
+      '- Number JavaScript Resources: ' + data.pageStats.numberJsResources + '\n' +
+      '- Number CSS Resources: ' + data.pageStats.numberCssResources + '\n'+
+      '- Avoid Landing Page Redirects: ' + JSON.stringify(data.formattedResults.ruleResults.AvoidLandingPageRedirects.ruleImpact) + '\n' +
+      '- Enable Gzip Compression: ' + JSON.stringify(data.formattedResults.ruleResults.EnableGzipCompression.ruleImpact) + '\n' +
+      '- Leverage Browser Caching: ' + JSON.stringify(data.formattedResults.ruleResults.LeverageBrowserCaching.ruleImpact) + '\n' +
+      '- Main Resource Server Response Time: ' + JSON.stringify(data.formattedResults.ruleResults.MainResourceServerResponseTime.ruleImpact) + '\n' +
+      '- Minify CSS: ' + JSON.stringify(data.formattedResults.ruleResults.MinifyCss.ruleImpact) + '\n' +
+      '- Minify HTML: ' + JSON.stringify(data.formattedResults.ruleResults.MinifyHTML.ruleImpact) + '\n' +
+      '- Minify JavaScript: ' + JSON.stringify(data.formattedResults.ruleResults.MinifyJavaScript.ruleImpact) + '\n' +
+      '- MinimizeRenderBlockingResources: ' + JSON.stringify(data.formattedResults.ruleResults.MinimizeRenderBlockingResources.ruleImpact) + '\n' +
+      '- Optimize Images: ' + JSON.stringify(data.formattedResults.ruleResults.OptimizeImages.ruleImpact) + '\n' +
+      '- Prioritize Visible Content: ' + JSON.stringify(data.formattedResults.ruleResults.PrioritizeVisibleContent.ruleImpact) + '\n';
+
+    mkdirp('performance-reports/desktop/', function (err) {
+      if (err) {
+        console.error(err)
+      } else {
+        fs.writeFile('performance-reports/desktop/test-desktop-' + fullDate + '.md', perfContent);
+        console.log('You can find the report: performance-reports/desktop/test-desktop-' + fullDate + '.md');
+      }
+    });
+
+    //console.log(data);
+  });
 });
 
 gulp.task('compass', function() {
@@ -91,5 +193,9 @@ gulp.task('watch', function () {
   });
 });
 
+
 // Default Task
 gulp.task('default', ['jslib', 'js','compass','templates', 'images', 'express','watch']);
+
+gulp.task('perf', ['speed-desktop', 'speed-mobile']);
+
